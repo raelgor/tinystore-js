b41b16ea67a18543772c68f2ed4d7bfb97dc562f
Dimitrios Kouzis-Loukas <lookfwd@gmail.com>
2015-09-06 01:15:38 +0100

    Minor fixes + tests. To install mocha `$ npm install -g mocha` (http://mochajs.org/). Then to run the tests just `$ mocha` on the root directory. Results:
    Bounce
      internal state after creation
        ✓ should be as expected
      #getPatternFromRequestObject()
        ✓ should match patterns with two keys
        ✓ shouldn't match patterns that don	 exist
        ✓ should match patterns with one key
      #getIdFromRequestObject()
        ✓ should create valid strings for patterns with two keys
        ✓ should return false for requests that don't match any pattern
        ✓ should create valid strings for patterns with one key
      #check()
        ✓ for invalid objects, by default it should return true
        ✓ for invalid objects, it should return false when defined so by using onUnknown()
        ✓ should work as expected while restoring original behaviour
        ✓ should ban more than 100 calls under the first rule
        ✓ should ban more than 101 calls under the second rule + full functional test
    
    12 passing (16ms)

--- src/utils/bounce.js
+++ src/utils/bounce.js
@@ -1,28 +1,24 @@
-// @todo:
-// http://stackoverflow.com/questions/12168708/is-there-any-limit-to-settimeout
-// settimeout from factory
-// testable
-// Limit #of requestIndex id's explicitly
-
 ﻿// Used to add patterns
 global.bounce = function (requestPattern) {
-
+    
     requestPattern = new bounce.RequestPattern(requestPattern);
 
     bounce.requestPatterns.push(requestPattern);
 
     return requestPattern;
-
 }
 
 // Wrap the pattern with this model to avoid conflicting with
 // the on, over and for keys
 bounce.RequestPattern = function (pattern) {
+    // Default values
+    this._on = 100;
+    this._over = 10000;
+    this._for = 10000;
 
     this.on = function (val) { this._on = val; return this; }
     this.over = function (val) { this._over = val; return this; }
     this.for = function (val) { this._for = val; return this; }
-    this.onUnknown = function (val) { this._onUnknown = val; return this; }
 
     this.pattern = pattern;
 
@@ -32,6 +28,24 @@ bounce.RequestPattern = function (pattern) {
 bounce.requestPatterns = [];
 bounce.requestIndex = {};
 bounce.blacklistIndex = {};
+bounce._onUnknown = true;
+
+bounce.resetMonitorStatus = function () {
+    this.requestIndex = {};
+    this.blacklistIndex = {};
+}
+
+bounce.reset = function () {
+    this.resetMonitorStatus();
+    this.requestPatterns = [];
+    this._onUnknown = true;
+}
+
+bounce.onUnknown = function (val) { bounce._onUnknown = val; }
+
+// Factory callback set to default setTimeout. Override for testability.
+// @todo Keep this in mind: http://stackoverflow.com/questions/12168708/is-there-any-limit-to-settimeout
+bounce.setTimeout = setTimeout;
 
 // Used to check requests
 bounce.check = function (requestObject) {
@@ -39,25 +53,26 @@ bounce.check = function (requestObject) {
     var requestID = this.getIdFromRequestObject(requestObject);
     var requestPattern = this.getPatternFromRequestObject(requestObject);
 
-    var blacklistIndex = this.blacklistIndex;
-    var requestIndex = this.requestIndex;
+    // If unrecognized request, take the default action
+    if (!requestID) return this._onUnknown;
 
-    // If unrecognized request, let it pass
-    if (!requestID) return requestPattern._onUnknown;
+    var blacklistIndex = this.blacklistIndex;
 
     // If requestID is banned, cut it
-    if (requestID in blacklistIndex) return _onUnknown;
-
+    if (requestID in blacklistIndex) return false;
+    
+    var requestIndex = this.requestIndex;
+    
     // Make or load this request's record
-    var record = requestIndex[requestID] || (requestIndex[requestID] = { status: 0, pool: 0 });
+    var record = requestIndex[requestID] || (requestIndex[requestID] = 0 );
 
     // If status is >= 1, add to blacklist
-    if (record.status >= 1) {
+    if (record >= requestPattern._on) {
 
         blacklistIndex[requestID] = 1;
 
         // Clear from blacklist after pattern's for
-        setTimeout(function () {
+        this.setTimeout(function () {
             delete blacklistIndex[requestID];
         }, requestPattern._for);
 
@@ -66,15 +81,12 @@ bounce.check = function (requestObject) {
     }
 
     // Update pool
-    record.pool++;
+    ++requestIndex[requestID];
 
     // Clear this request from pool after time specified by pattern
-    setTimeout(function () {
-        if(--record.pool == 0) delete requestIndex[requestID];
+    this.setTimeout(function () {
+        if(--requestIndex[requestID] == 0) delete requestIndex[requestID];
     }, requestPattern._over);
-
-    // Update status
-    record.status = record.pool / requestPattern._on;
 
     // If we got here, pass
     return true;
@@ -83,29 +95,31 @@ bounce.check = function (requestObject) {
 
 // Used to find the pattern of a request
 bounce.getPatternFromRequestObject = function (requestObject) {
+    var result = false;
     
-    var matchPattern;
-
     this.requestPatterns.forEach(function (RequestPatternObject) {
 
         var pattern = RequestPatternObject.pattern;
-        var fail;
+        var success = true;
 
+        // I think that an or-logic was required here - but
+        // "and" logic is implemented instead
         for (var key in pattern.$match) {
 
             if (pattern.$match[key] != requestObject[key]) {
 
-                fail = 1;
+                success = false;
 
             }
 
         }
-
-        matchPattern = !fail && RequestPatternObject;
-
+        
+        if (!result && success) {
+            result = RequestPatternObject;
+        }
     });
 
-    return matchPattern || false;
+    return result;
 
 }
 
@@ -113,9 +127,10 @@ bounce.getPatternFromRequestObject = function (requestObject) {
 bounce.getIdFromRequestObject = function (requestObject) {
 
     var patternWrapper = this.getPatternFromRequestObject(requestObject);
-    var requestID = [];
 
     if (!patternWrapper) return false;
+    
+    var requestID = [];
 
     var pattern = patternWrapper.pattern;
 
