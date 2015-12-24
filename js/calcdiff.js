1099d7c8c3c7ef2ae90f3a4137cea54c9d932aad
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2015-12-25 01:51:01 +0200

    Fixed trim error bug

--- js/calcdiff.js
+++ js/calcdiff.js
@@ -1,11 +1,13 @@
 ﻿setTimeout(function () {
-
+    
+    if(!$('.item-content-wrapper .old-price span:first-child').html()) return;
+    
     var oldPrice = +$('.item-content-wrapper .old-price span:first-child').html().trim() || NaN;
     var newPrice = +$('.item-content-wrapper .new-price span:nth-child(2)').html().trim() || NaN;
 
     if (oldPrice && newPrice && oldPrice !== newPrice) {
 
-        var pc = -(parseInt(newPrice / oldPrice * 100) - 100);
+        var pc = -(Math.floor(newPrice / oldPrice * 100) - 100);
         var dif = String(Math.round((oldPrice - newPrice) * 100) / 100);
 
         $('.item-content-wrapper .new-price').after('<div class="diff">Κερδίζετε ' + dif + '&euro; (-' + pc + '%)</div>')
